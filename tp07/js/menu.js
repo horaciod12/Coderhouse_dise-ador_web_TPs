7950695fc12f8f17fe97fbc25da0a9689fac6961
horaciod12 <daddarioh@gmail.com>
2017-03-21 23:59:12 -0300

    Primer subida TPs

---
+++ tp07/js/menu.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById('menu');
+var ul = document.getElementById('ul');
+
+menu.addEventListener('click', function() {
+	ul.classList.toggle('mostrar');
+})
---
+++ tp08/js/menu.js
@@ -0,0 +1,7 @@
+var menu = document.getElementById('menu');
+var ul = document.getElementById('ul');
+
+menu.addEventListener('click', function() {
+	console.log('jgchgc');
+	ul.className = 'mostrar';
+})
